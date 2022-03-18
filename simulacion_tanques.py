477eba502bb52da88696298423c9405889dc7b99
Tomás Varela <tomas.varela@gmail.com>
2022-03-18 16:24:43 +0100

    sim solo tanque out

--- simulacion_tanques.py
+++ simulacion_tanques.py
@@ -13,18 +13,20 @@ class Tank:
         self.level -= qoutlet / self.size
 
 outTank = Tank(50, 5, 7)
+inTank = Tank(50, 2, 3)
 qin = 90
 qout = 120
 pumpout = False
-while True:
-    outTank.fill(qin)
 
+while True:
     if outTank.level > outTank.highsw:
         pumpout = True
     if outTank.level < outTank.lowsw:
         pumpout = False
 
+    outTank.fill(qin)
     if pumpout == True:
         outTank.empty(qout)
-    print(" Tank level: {}/n Pump: {}".format(outTank.level, pumpout))
+
+    print(" Tank level: {} \n Pump: {} \n -------------".format(outTank.level, pumpout))
     time.sleep(1)
